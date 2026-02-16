b1bac4aac87178083db1d78267cdafa735da1bef
Kevin Fowler <kevin.m.fowler17@gmail.com>
2026-02-16 14:38:24 -0600

    Initial Spring Boot recipes project setup

---
+++ src/main/java/com/kevinfowler/recipes/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.kevinfowler.recipes;
+
+import com.mythologyproject.mythology_project.services.UserDetailsLoader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private final UserDetailsLoader usersLoader;
+
+    public SecurityConfiguration(UserDetailsLoader usersLoader) {
+        this.usersLoader = usersLoader;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf(csrf -> csrf.enable()) // Disable CSRF if testing, enable in production
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/glossary", "/subject-card", "/register", "/about-me", "/audio/**", "/css/**", "/img/**", "/js/**").permitAll()
+                        .requestMatchers("/user", "/user/{id}/quiz").authenticated()
+                )
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/login")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
